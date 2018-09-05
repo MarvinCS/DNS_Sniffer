4fc4d7d31a84b253aae18f577c3570ee80aa72c2
Marvin <marvincschneider@gmail.com>
2018-09-05 17:20:15 +0200

    choose your interface

--- main.py
+++ main.py
@@ -6,30 +6,40 @@ You need root-permissions to execute this code
 '''
 
 
-def startMonitorMode(interface='wlan1'):
-    os.system('ifconfig ' + interface + ' down')
-    os.system('iw dev ' + interface + ' interface add mywlanmonitor type monitor')
+def startMonitorMode(_interface='wlan1'):
+    os.system('ifconfig ' + _interface + ' down')
+    os.system('iw dev ' + _interface + ' interface add mywlanmonitor type monitor')
     os.system('ifconfig mywlanmonitor down')
     os.system('iw dev mywlanmonitor set type monitor')
     os.system('ifconfig mywlanmonitor up')
 
 
-def stopMonitorMode(interface='wlan1'):
+def stopMonitorMode(_interface='wlan1'):
     os.system('ifconfig mywlanmonitor down')
     os.system('iw dev mywlanmonitor del')
-    os.system('ifconfig ' + interface + ' up')
+    os.system('ifconfig ' + _interface + ' up')
     os.system('service network-manager restart')
 
 
-def captureDNS(interface='mywlanmonitor'):
-    capture = pyshark.LiveCapture(interface=interface, bpf_filter='udp port 53')
+def chooseInterface():
+    print('Available network-interfaces:')
+    os.system('ifconfig -a | sed \'s/[ \t].*//;/^\(lo\|\)$/d\'')
+    return input('\nWhich one do you want to use?\n')
+
+
+def captureDNS(_interface='mywlanmonitor'):
+    capture = pyshark.LiveCapture(interface=_interface, bpf_filter='udp port 53')
     for packet in capture.sniff_continuously(packet_count=2):
         if 'DNS Layer' in str(packet.layers):
-            print("Source: " + packet.ip.src)
-            print("Target: " + packet.ip.dst)
+            try:
+                print("Source: " + packet.ip.src)
+                print("Target: " + packet.ip.dst)
+            except AttributeError:
+                print("IP-informations are not available")
             print(packet.dns)
 
 
-startMonitorMode('wlp5s0')
+interface = chooseInterface()
+startMonitorMode(interface)
 captureDNS()
-stopMonitorMode('wlp5s0')
+stopMonitorMode(interface)
